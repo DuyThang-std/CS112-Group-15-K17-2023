01e32b55b87df2e9d3fb5b0337182e7ccb836cc2
tamtam24 <109749613+tamtam24@users.noreply.github.com>
2023-10-19 19:28:21 +0700

    Add files via upload
    code bài password

---
+++ Thảo luận/password.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = set(map(int, input().split()))
+count = 0
+for i in range(10000):
+	num = str(i)
+	num = (4 - len(num)) * '0' + num
+	used = set(num)
+	if len(used) != 2:
+		continue
+	d1 = int(used.pop())
+	d2 = int(used.pop())
+	if (not d1 in a) and (not d2 in a) and num.count(num[0]) == 2:
+		count += 1
+print(count)
